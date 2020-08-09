c9f4608daa86c213cb4d9265b6c6f5a1374e31b9
zjt <1571510352@qq.com>
2020-08-09 17:04:36 +0800

    修改a标签

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,49 @@
 <template>
-<div>
-   <div id="app">
+  <div>
+    <div id="app">
       <!-- 命名路由 -->
       <router-link :to="{name:'home'}">发现音乐</router-link>
       <router-link :to="{name:'music'}">我的音乐</router-link>
-      <router-link :to="{name:'login'}" v-if='showname'>登录</router-link>
+      <router-link :to="{name:'login'}" v-if="showname">登录</router-link>
       <a href="/home" @click="exit">退出</a>
-   </div>
-   <keep-alive>
-     <router-view></router-view>
-   </keep-alive>
-</div>
+    </div>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data(){
-    return{
-      showname:true
-    }
+  name: "App",
+  data() {
+    return {
+      showname: true,
+    };
   },
-  methods:{
-    exit(){
-      console.log('清除啦');
+  methods: {
+    exit() {
+      console.log("清除啦");
       localStorage.clear();
+    },
+  },
+  updated() {
+    if (localStorage.getItem("user")) {
+      this.showname = false;
+    } else {
+      this.showname = true;
     }
   },
-  updated(){
-    if(localStorage.getItem('user')){
-      this.showname=false;
-    }else{
-      this.showname=true;
-    }
-  }
-}
+};
 </script>
 <style>
-  #app{
-    width: 100%;
-    height: 30px;
-    padding: 10px;
-    background-color: rgb(240, 193, 124);
-  }
-  a{
-        margin-left: 20px;
-    }
+#app {
+  width: 100%;
+  height: 30px;
+  padding: 10px;
+  background-color: rgb(240, 193, 124);
+}
+a {
+  margin-right: 20px;
+}
 </style>
